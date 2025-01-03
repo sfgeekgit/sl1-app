c3a1090708939c86b469c3728be7294c06df8b7e
Nick Shapiro <141788755+sfgeekgit@users.noreply.github.com>
2025-01-03 02:09:45 +0000

    First commit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new streamlit app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This works! [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+st.write(
+    "The ooonly limit is [https://zombo.com](zombo.com)."
 )
